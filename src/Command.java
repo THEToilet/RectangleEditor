48477b63aba860c13ddda3861f3fd8eeb0510943
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-18 23:14:22 +0900

    :sparkles: Add Command switch

--- src/Command.java
+++ src/Command.java
@@ -3,14 +3,47 @@ import java.util.Scanner;
 public class Command {
 
   enum Action {
-    NONE, CREATE, MOVE, EXPAND, SHRINK, DELETE, DELETEALL, INTERSEC, DISPLAYBOARD, EXIT
+    CREATE, MOVE, EXPAND, SHRINK, DELETE, DELETEALL, INTERSEC, DISPLAYBOARD, EXIT, NONE
   }
 
   public static void Start() {
+    String acString = "0:CREATE 1:MOVE 2:EXPAND 3:SHRINK 4:DELETE\n 5:DELETEALL 6:INTERSEC 7:DISPLAYBOARD 8: EXIT\n";
+
     Action action = Action.NONE;
+
     while (action != Action.EXIT) {
+      System.out.printf(acString);
+      System.out.printf(">> ");
       Scanner scanner = new Scanner(System.in);
       String str = scanner.nextLine();
+
+      switch (action) {
+        case CREATE:
+          break;
+        case MOVE:
+          break;
+        case EXPAND:
+          break;
+        case SHRINK:
+          break;
+        case DELETE:
+          break;
+        case DELETEALL:
+          break;
+        case INTERSEC:
+          break;
+        case DISPLAYBOARD:
+          break;
+        case EXIT:
+          System.out.println("exit ..");
+          break;
+        case NONE:
+          break;
+        default:
+          break;
+      }
+
+      System.out.println(str);
       scanner.close();
     }
   }
