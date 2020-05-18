a64cb47385462e0be87695415155e316691bc5b9
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-18 22:40:29 +0900

    :sparkles: Define enum action

---
+++ src/Command.java
@@ -0,0 +1,13 @@
+public class Command {
+
+  enum Action {
+    NONE, CREATE, MOVE, EXPAND, SHRINK, DELETE, DELETEALL, INTERSEC, DISPLAYBOARD, EXIT
+  }
+
+  public void Start() {
+    Action action = Action.NONE;
+    while (action != Action.EXIT) {
+
+    }
+  }
+}
