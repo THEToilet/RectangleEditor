c7248f31efc9ef915fa78c0743ae0706c567f2de
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-19 00:03:06 +0900

    :sparkles: Add rectangles Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,10 @@
-public class Board {
+import java.util.ArrayList;
 
-  
+public class Board {
+  ArrayList<Rectangle> rectangles = new ArrayList<>();
+
+  public void addRectangle(Rectangle rectangle) {
+    rectangles.add(rectangle);
+  }
+
 }
