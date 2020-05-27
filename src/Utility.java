6a16a4e8b1efca35cb02afc182983f80b3537598
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-28 00:19:06 +0900

    :sparkles: Add intersec

--- src/Utility.java
+++ src/Utility.java
@@ -5,12 +5,23 @@ public class Utility {
     Rectangle r1 = Board.getRectangl(rec1);
     Rectangle r2 = Board.getRectangl(rec2);
     if (isOverlapping(r1, r2)) {
-      Rectangle rectangle = new Rectangle(Math.max(r1.x, r2.x), Math.min(r1.y, r2.y), Math.abs(Math.max(r1.x, r2.x)),
-          Math.abs(Math.max(r1.x, r2.x)), new Color(combineColors(r1.color, r2.color)));
-      Board.deleteRectangle(rec1);
-      Board.deleteRectangle(rec2 - 1);
-      Board.addRectangle(rectangle);
-      System.out.println("It worked.");
+
+      int sx = Math.max(r1.x + r1.width / 2, r2.x + r2.width / 2);
+      int sy = Math.max(r1.y + r1.height / 2, r2.y + r2.height / 2);
+      int ex = Math.min(r1.x + r1.width / 2 + r1.width, r2.x + r2.width / 2 + r2.width);
+      int ey = Math.min(r1.y + r1.height / 2 + r1.height, r2.y + r2.height / 2 + r2.height);
+
+      int w = ex - sx;
+      int h = ey - sy;
+      if (w > 0 && h > 0) {
+        Rectangle rectangle = new Rectangle(sx, sy, w, h, new Color(combineColors(r1.color, r2.color)));
+        Board.deleteRectangle(rec1);
+        Board.deleteRectangle(rec2 - 1);
+        Board.addRectangle(rectangle);
+        System.out.println("It worked.");
+      } else {
+        System.out.println("It didn't work.");
+      }
     } else {
       System.out.println("It didn't work.");
     }
