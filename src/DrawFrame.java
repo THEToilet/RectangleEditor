9552b54fbdf6f8c1e2f97751acbef541958b5fc0
THEToilet <bp18015@shibaura-it.ac.jp>
2020-06-01 15:48:28 +0900

    :sparkles: Add DrawFrame

---
+++ src/DrawFrame.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class DrawFrame extends Frame {
+  DrawFrame(String title) {
+  this.setTitle(title);
+  this.addWindowListener(new WindowAdapter() {
+  public void windowClosing(WindowEvent e) {
+  System.exit(0);
+  }
+  });
+  this.setLocation(300, 100);
+  this.setSize(500, 500);
+  this.setBackground(Color.white);
+  }
+
+  public void paint(Graphics g) {
+  }
+}
--- src/RectangleEditor.java
+++ src/RectangleEditor.java
@@ -1,6 +1,8 @@
 public class RectangleEditor {
 
 	static public void main(String[] args) {
+		DrawFrame frm = new DrawFrame("Rectangle Editor");
+		frm.setVisible(true);
 		System.out.printf("Hello %s! This is the Rectangle Editor\n", System.getProperty("user.name"));
 		System.out.printf("Please Type in commands\n");
 		Command.Start();
